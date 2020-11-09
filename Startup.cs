54d10f6ff57d7c012bc02b52424b9ac8a8fe97a4
Bhumi712 <73623413+Bhumi712@users.noreply.github.com>
2020-11-09 19:00:18 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BhumiMehta_WebMobi.Startup))]
+namespace BhumiMehta_WebMobi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
